a80ec6d9a05eeef9c522dec36e56365e6db00ed1
JMMoir <jmmoir@outlook.com>
2019-05-31 11:02:04 +0100

    basic files setup

---
+++ src/test/java/PassengerTest.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+
+public class PassengerTest {
+
+    private Passenger passenger;
+
+    @Before
+
+    public void setUp() {
+        passenger = new Passenger("John", 1);
+    }
+
+}
